e1b01cd8398ca4a3a5e88f924d860f6a229d8033
Guest <guest-lomw4l@h26682.sgb.skir.kr>
2021-01-23 20:05:27 +0900

    Revert "complete All"
    This reverts commit 4375cde20f8dd4e0bc3f868ccca61563c9e224d6.

--- visual.js
+++ visual.js
@@ -39,22 +39,6 @@ export class Visual{
         for(let i = 0 ; i< this.particles.length; i++){
             const item = this.particles[i];
 
-            const dx = this.mouse.x - item.x;
-            const dy = this.mouse.y - item.y;
-            const dist = Math.sqrt(dx * dx + dy * dy);
-            const minDist = item.radius + this.mouse.radius;
-
-            if(dist < minDist){
-                const angle = Math.atan2(dy, dx);
-                const tx = item.x + Math.cos(angle) * minDist;
-                const ty = item.y + Math.sin(angle) * minDist;
-                const ax = tx - this.mouse.x;
-                const ay = ty - this.mouse.y;
-                item.vx -= ax;
-                item.vy -= ay;
-                item.collide();
-            }
-
             item.draw(ctx, t);
         }
     }
